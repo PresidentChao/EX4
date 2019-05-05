9e9e30ae6f4a0fe5c9efa8d3b47a77558a14527b
PresidentChao <781910191@qq.com>
2019-05-05 22:27:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/ex4/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.lenovo.ex4;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //加载Fragment
+        FragmentManager fragmentManager=getFragmentManager();
+        //开启一个新事务
+        FragmentTransaction transaction=fragmentManager.beginTransaction();
+        PreFragment preFragment=new PreFragment();
+        transaction.replace(android.R.id.content,preFragment);
+        transaction.commit();
+    }
+}
